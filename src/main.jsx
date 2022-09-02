a2de5eb276d5053542038ae3bae4705abd723fdd
Folareen <sakawahab03@gmail.com>
2022-09-02 23:14:09 +0100

    added redux provider

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './styles/css/index.css'
-import Provider from 'react-redux'
+import {Provider }from 'react-redux'
 import store from './app/store'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
